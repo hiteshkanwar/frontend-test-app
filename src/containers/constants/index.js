3784728387058b8cd932c70a80248e7840be4ab4
hiteshkanwar <Hiteshgurjar786@gmail.com>
2019-07-04 17:15:16 +0530

    added url

--- src/containers/constants/index.js
+++ src/containers/constants/index.js
@@ -1,6 +1,6 @@
 // put the values which is constant over the app.
  export const appConstants = {
    // WEB_SERVICE_URL: 'https://xyz.com',
-   WEB_SERVICE_URL: 'https://25180a86.ngrok.io/api/v1',
+   WEB_SERVICE_URL: 'https://rails-backend-api.herokuapp.com/api/v1',
 
   }
